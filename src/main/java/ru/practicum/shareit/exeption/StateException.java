8ca1a12602998d41e463ce790ab3c831c55aa4e2
Dw4yn33 <vova-kalinich@mail.ru>
2023-09-19 13:16:19 +0300

    Спринт 14. Попытка №2.

---
+++ src/main/java/ru/practicum/shareit/exeption/StateException.java
@@ -0,0 +1,9 @@
+package ru.practicum.shareit.exeption;
+
+public class StateException extends RuntimeException {
+
+    public StateException(String message) {
+        super(message);
+    }
+
+}
---
+++ src/main/java/ru/practicum/shareit/user/service/impl/UserServiceImpl.java
@@ -0,0 +1,63 @@
+package ru.practicum.shareit.user.service.impl;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+import ru.practicum.shareit.exeption.NotFoundException;
+import ru.practicum.shareit.user.dto.UserDto;
+import ru.practicum.shareit.user.mapper.UserMapper;
+import ru.practicum.shareit.user.model.User;
+import ru.practicum.shareit.user.repository.UserRepository;
+import ru.practicum.shareit.user.service.UserService;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@AllArgsConstructor
+public class UserServiceImpl implements UserService {
+
+    private final UserMapper userMapper;
+    private final UserRepository userRepository;
+
+    @Override
+    public List<UserDto> getAll() {
+        return userRepository.findAll().stream()
+                .map(userMapper::toDto)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public UserDto findById(Long userId) {
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new NotFoundException(String.format("User with id %x not found!", userId)));
+
+        return userMapper.toDto(user);
+    }
+
+    @Override
+    public UserDto create(UserDto userDto) {
+        return userMapper.toDto(userRepository.save(userMapper.toUser(userDto)));
+    }
+
+    @Override
+    public UserDto update(Long userId, UserDto userDto) {
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new NotFoundException(String.format("User with id %x not found!", userId)));
+
+        if (userDto.getEmail() != null) {
+            user.setEmail(userDto.getEmail());
+        }
+
+        if (userDto.getName() != null) {
+            user.setName(userDto.getName());
+        }
+
+        return userMapper.toDto(userRepository.save(user));
+    }
+
+    @Override
+    public void delete(Long userId) {
+        userRepository.deleteById(userId);
+    }
+
+}
