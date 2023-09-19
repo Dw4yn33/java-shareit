6379d8be4a3468bd0372089c8816abf43b681096
Dw4yn33 <vova-kalinich@mail.ru>
2023-09-19 12:39:56 +0300

    Спринт 14. Решил полностью изменить структуру проекта.

--- src/main/java/ru/practicum/shareit/exceptions/UnexpectedException.java
+++
@@ -1,8 +0,0 @@
-package ru.practicum.shareit.exceptions;
-
-public class UnexpectedException extends RuntimeException {
-
-    public UnexpectedException(String message) {
-        super(message);
-    }
-}
--- src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
+++ src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
@@ -1,26 +1,40 @@
 package ru.practicum.shareit.item.dto;
 
-import lombok.Data;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import ru.practicum.shareit.booking.dto.BookingItemDto;
+import ru.practicum.shareit.request.dto.ItemRequestDto;
+import ru.practicum.shareit.user.dto.UserDto;
 
-/**
- * TODO Sprint add-controllers.
- */
-@Data
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.util.List;
+
+@Getter
+@Setter
+@Builder
 public class ItemDto {
 
     private Long id;
-    //@NotBlank
-    private String name;
-    //@NotBlank
-    private String description;
-    //@NotNull
-    private Boolean available;
-    //private long requestId;
 
-    public ItemDto(Long id, String name, String description, Boolean available) {
-        this.id = id;
-        this.name = name;
-        this.description = description;
-        this.available = available;
-    }
+    @NotBlank
+    private String name;
+
+    @NotBlank
+    private String description;
+
+    @NotNull
+    private Boolean available;
+
+    private UserDto owner;
+
+    private ItemRequestDto request;
+
+    private BookingItemDto lastBooking;
+
+    private BookingItemDto nextBooking;
+
+    private List<CommentDto> comments;
+
 }
