c1ea95dcc979b17611b800b967bfca6de2ee303e
Dw4yn33 <vova-kalinich@mail.ru>
2023-10-01 19:43:12 +0300

    Спринт 15. Попытка №2.

--- src/main/java/ru/practicum/shareit/exception/AlreadyExistsException.java
+++ src/main/java/ru/practicum/shareit/exception/AlreadyExistsException.java
@@ -2,5 +2,7 @@ package ru.practicum.shareit.exception;
 
 public class AlreadyExistsException extends RuntimeException {
 
-    public AlreadyExistsException(String message) { super(message); }
+    public AlreadyExistsException(String message) {
+        super(message);
+    }
 }
